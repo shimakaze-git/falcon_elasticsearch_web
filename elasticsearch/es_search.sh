dc579ce3de233479af45ed4a039ea03c2a9ac262
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-04-16 00:58:04 +0900

    fix: fix ping && search shellscript files

--- elasticsearch/es_search.sh
+++ elasticsearch/es_search.sh
@@ -1,3 +1,3 @@
-curl http://localhost:9200/my_index/user/_search?pretty
+curl 'http://localhost:9200/my_index/user/_search?pretty'
 
-curl http://localhost:9200/test_index/user/_search?pretty
+curl 'http://localhost:9200/test_index/user/_search?pretty'
