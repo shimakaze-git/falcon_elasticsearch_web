68c7061970a945a8d920a11d3b064154e0f642f4
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-04-16 00:14:15 +0900

    feat: add docker-compose container files

---
+++ elasticsearch/es_ping.sh
@@ -0,0 +1,2 @@
+curl -X GET http://localhost:9200
+curl -X GET http://localhost:9200/_nodes/plugins
---
+++ elasticsearch/insert_index.sh
@@ -0,0 +1,3 @@
+curl -X POST -H 'Content-Type: application/json' http://localhost:9200/test_index/user -d '{"name":"hoge"}'
+
+curl -X POST -H 'Content-Type: application/json' http://localhost:9200/my_index/user -d '{"name":"Yohei"}'
